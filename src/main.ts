0aa1cc80809e8397715d484a6bfc8caa296b32a5
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-11-23 18:38:42 -0300

    animation efx

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,14 @@ const testBtn = document.querySelector("#test-btn") as HTMLButtonElement;
 //   battleLanesUI[3].children,
 // ].map((HTMLels) => Array.from(HTMLels));
 
+let turnSequence: any[] = [];
+let currentTurn = null;
+let turnCount = 0;
+let battleStarted = false;
+let ongoingAttack = false;
+
 testBtn.onclick = () => {
+  if (!battleStarted || ongoingAttack) return;
   updateTurnSequence();
 };
 
@@ -129,13 +136,10 @@ const heroes = [
 
 const allCharacters = [...enemies, ...heroes];
 
-let turnSequence: any[] = [];
-let currentTurn = null;
-let turnCount = 0;
+type Character = (typeof allCharacters)[0];
 
 function idMaker(length = 12) {
-  const ID_CHARS =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+  const ID_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
   return Array(length)
     .fill(0)
     .map((_) => ID_CHARS.split("")[Math.round(Math.random() * ID_CHARS.length)])
@@ -150,7 +154,7 @@ function calculateNextTurnTime(turn: any) {
   return turn.nextTurnAt + turn.turnDuration;
 }
 
-function chooseTargetForEnemy(enemy: any) {
+function chooseTargetForEnemy(enemy: Character) {
   let possibleTargets: any[];
 
   if (enemy.actions.attack.type === "ranged") {
@@ -173,7 +177,7 @@ function chooseTargetForEnemy(enemy: any) {
   return possibleTargets[idx];
 }
 
-function drawCharacter(entity: any) {
+function drawCharacter(entity: Character) {
   const battleLane = battleLanesUI.find(
     (el) =>
       el.classList.contains(`${entity.type}-lane`) &&
@@ -192,7 +196,11 @@ function drawCharacter(entity: any) {
   const slot = Array.from(battleLane.children)[slotIdx];
   const [topSection, avatar, bottomSection] = Array.from(slot.children);
   const img = avatar.children[0] as HTMLImageElement;
+  // const img = Array.from(avatar.children).find(
+  //   (el) => el.tagName === "IMG"
+  // ) as HTMLImageElement;
 
+  slot.id = `${entity.id}`;
   topSection.textContent = entity.name;
   bottomSection.textContent = `HP ${entity.hp}`;
   img.src = entity.imgUrl;
@@ -200,6 +208,9 @@ function drawCharacter(entity: any) {
 
 function drawCharacters() {
   allCharacters.forEach(drawCharacter);
+
+  // const activeCharacterSlot: string = currentTurn?.entityId ?? "";
+  // console.log({ activeCharacterSlot, currentTurn });
 }
 
 function initializeTurnSequence() {
@@ -215,8 +226,10 @@ function initializeTurnSequence() {
 
   drawTurnSequence();
 
+  // run initial turn
   setTimeout(() => {
     turnCount++;
+    battleStarted = true;
     handleCharacterTurn(allCharacters[0]);
   }, 2000);
 }
@@ -246,14 +259,14 @@ function updateTurnSequence() {
 
   const character = allCharacters.find(
     (c) => c.id === turnSequence[0].entityId
-  );
+  )!;
 
   handleCharacterTurn(character);
   drawTurnSequence();
 }
 
 function drawTurnSequence() {
-  console.log(turnCount, ...turnSequence);
+  // console.log(turnCount, ...turnSequence);
   turnSequenceUI.innerHTML = "";
 
   for (let i = 0; i < turnSequence.length; i++) {
@@ -279,25 +292,102 @@ function drawTurnSequence() {
   }
 }
 
-function handleCharacterTurn(entity: any) {
+async function drawSelectedCharacterOutline(entity: Character) {
+  const prevSlot = document.querySelector(`.selected`);
+  const slot = document.querySelector(`#${entity.id}`);
+
+  prevSlot?.classList.remove("selected");
+  slot?.classList.add("selected");
+
+  console.log(`${entity.name}'s turn...`);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 750);
+  });
+}
+
+async function handleCharacterTurn(entity: Character) {
   console.log(`it's ${entity.name}'s turn`);
 
+  await drawSelectedCharacterOutline(entity);
+
   if (entity.type === "enemy") {
-    const target = chooseTargetForEnemy(entity);
+    const targetHero = chooseTargetForEnemy(entity);
     console.log(
-      `${entity.name} performs a ${entity.actions.attack.type} attack against ${target.name}`
+      `${entity.name} performs a ${entity.actions.attack.type} attack against ${targetHero.name}`
     );
+
+    await handleAttack(entity, targetHero);
+
+    console.log("done!");
   } else if (entity.type === "hero") {
     // chooseRandomEntity(enemies);
   }
+}
+
+async function handleAttack(attacker: Character, target: Character) {
+  ongoingAttack = true;
+  const attackPower = attacker.actions.attack.power;
+
+  await drawAttackEffect(attacker, target);
+
+  target.hp -= attackPower;
+  drawCharacters();
+  ongoingAttack = false;
+}
+
+async function drawAttackEffect(attacker: Character, target: Character) {
+  const targetSlot = document.querySelector(`#${target.id}`)!;
+  const attackerSlot = document.querySelector(`#${attacker.id}`)!;
+
+  // const targetAvatarEl = Array.from(targetSlot?.children || []).find((el) =>
+  //   el.classList.contains("avatar")
+  // )!;
+
+  // const attackerAvatarEl = Array.from(attackerSlot?.children || []).find((el) =>
+  //   el.classList.contains("avatar")
+  // )!;
+
+  // const [targetImg, targetOverlay] = Array.from(targetAvatarEl?.children || []);
+  // const [attackerImg, attackerOverlay] = Array.from(
+  //   attackerAvatarEl?.children || []
+  // );
+
+  attackerSlot.classList.add(`${attacker.actions.attack.type}-attack-perform`);
+  targetSlot.classList.add(`${attacker.actions.attack.type}-attack-receive`);
+
+  // return new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      attackerSlot.classList.remove(
+        `${attacker.actions.attack.type}-attack-perform`
+      );
+      targetSlot.classList.remove(
+        `${attacker.actions.attack.type}-attack-receive`
+      );
+
+      console.log("removed classes");
+      return resolve(true);
+    }, 1000);
+  });
+
+  // console.log({
+  //   attackerSlot,
+  //   targetSlot,
+  //   attackerImg,
+  //   attackerOverlay,
+  //   targetImg,
+  //   targetOverlay,
+  // });
 }
 
 function main() {
   drawCharacters();
 
   initializeTurnSequence();
-
-  // setInterval(updateTurnSequence, 2000);
 }
 
 main();
